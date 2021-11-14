9267eae7067664ffed0e5995cab19280a320e287
tony <antolisah@gmail.com>
2021-11-14 08:48:21 +0300

    asset colors and fonts

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import colors from './assets/colors/colors';
 
 export default function App() {
+  const [loaded] = useFonts({
+    OpenSans: require('./assets/fonts/OpenSans-Regular.ttf'),
+    OpenSansBold: require('./assets/fonts/OpenSans-Bold.ttf'),
+    OpenSansSemiBold: require('./assets/fonts/OpenSans-SemiBold.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: 'OpenSansSemiBold', fontSize: 30, color: colors.blue }}>Wassup!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ assets/colors/colors.js
@@ -0,0 +1,8 @@
+const colors = {
+  black: '#000000',
+  white: '#ffffff',
+  gray: '#666666',
+  blue: '#1c215d',
+};
+
+export default colors;
